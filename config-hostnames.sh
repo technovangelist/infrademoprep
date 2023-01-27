f3d2ade94564567c85c16d166ef00cab66f3fdc3
Matt Williams <m@technovangelist.com>
2023-01-27 15:55:36 -0800

    added nginx.yaml to .
    Signed-off-by: Matt Williams <m@technovangelist.com>

--- config-hostnames.sh
+++ config-hostnames.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/env bash
+export CIVO_RESERVED_IP=212.2.243.0
+export DO_RESERVED_IP=143.198.246.247
 export CIVO_SSH_HOSTNAME=utils-civo
 export DO_SSH_HOSTNAME=cicd-do
 export AWS_SSH_HOSTNAME=qa-aws
