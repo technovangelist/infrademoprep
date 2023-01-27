5ff83a2ea524919449cac55c9f10fd8867da4dc4
Matt Williams <m@technovangelist.com>
2023-01-27 09:53:39 -0800

    modified resetdemo.sh in .
    Signed-off-by: Matt Williams <m@technovangelist.com>

--- resetdemo.sh
+++ resetdemo.sh
@@ -6,5 +6,6 @@ infra grants remove richard@infrahq.com $CIVO_SSH_HOSTNAME
 infra grants remove richard@infrahq.com $DO_SSH_HOSTNAME
 infra grants remove richard@infrahq.com $CIVO_K8S_NAME
 infra grants remove richard@infrahq.com $DO_K8S_NAME
+infra groups removeuser richard@infrahq.com Developers
 
 ssh richard@rocky rm /home/richard/.kube/config
