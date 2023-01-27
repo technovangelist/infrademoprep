78d6a705c9ec56e2ccc6f1478df1a0cbaf36efd8
Matt Williams <m@technovangelist.com>
2023-01-27 09:55:40 -0800

    modified resetdemo.sh in .
    Signed-off-by: Matt Williams <m@technovangelist.com>

--- resetdemo.sh
+++ resetdemo.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 echo Setting the Connection Names
-source ./config-hostnames.sh
+source /Users/demo/.bin/config-hostnames.sh
 
 infra grants remove richard@infrahq.com $CIVO_SSH_HOSTNAME
 infra grants remove richard@infrahq.com $DO_SSH_HOSTNAME
