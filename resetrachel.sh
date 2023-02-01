0fc37331ae442a73c9cbca8f8cfe57f08faab221
Matt Williams <m@technovangelist.com>
2023-01-31 17:34:03 -0800

    changed 4 files
    Signed-off-by: Matt Williams <m@technovangelist.com>

--- resetrachel.sh
+++ resetrachel.sh
@@ -4,11 +4,6 @@ cat /Users/demo/demoprep/client/resetdemo.sh
 echo Configuring the Connection Names
 source /Users/demo/.bin/config-hostnames.sh
 
-echo Removing Richards Grants
-infra grants remove richard@infrahq.com $CIVO_SSH_HOSTNAME
-infra grants remove richard@infrahq.com $DO_SSH_HOSTNAME
-infra grants remove richard@infrahq.com $CIVO_K8S_NAME
-infra grants remove richard@infrahq.com $DO_K8S_NAME
 
 echo Removing Rachels Grants
 infra grants remove rachel@infrahq.com $CIVO_SSH_HOSTNAME
@@ -16,23 +11,17 @@ infra grants remove rachel@infrahq.com $DO_SSH_HOSTNAME
 infra grants remove rachel@infrahq.com $CIVO_K8S_NAME
 infra grants remove rachel@infrahq.com $DO_K8S_NAME
 
-echo Removing Richard and Rachel from Dev Group
-infra groups removeuser richard@infrahq.com Developers
+echo Removing Rachel from Dev Group
 infra groups removeuser rachel@infrahq.com Developers
 
 echo Making sure Developers still have Edit Role
 infra grants add -g Developers $CIVO_K8S_NAME --role edit
 infra grants add -g Developers $DO_K8S_NAME --role edit
 
-echo Clean up Richards VM
-# ssh richard@rocky rm /home/richard/.kube/config
-scp /Users/demo/demoprep/client/nginx.yaml richard@rocky:/home/richard
-ssh richard@rocky kubectl delete -f /home/richard/nginx.yaml
-ssh richard@rocky infra logout
-
 echo Clean up Rachels VM
 # ssh richard@rocky rm /home/richard/.kube/config
-scp /Users/demo/demoprep/client/nginx.yaml rachel@rocky:/home/rachel
+scp /Users/demo/demoprep/client/nginx-rachel.yaml rachel@rocky:/home/rachel
+ssh richard@rocky kubectl delete -f /home/richard/nginx-rachel.yaml
 ssh rachel@rocky infra logout
 
 echo Reset Complete
---
+++ resetrichard.sh
@@ -0,0 +1,29 @@
+#! /usr/bin/env bash
+cat /Users/demo/demoprep/client/resetdemo.sh
+
+echo Configuring the Connection Names
+source /Users/demo/.bin/config-hostnames.sh
+
+echo Removing Richards Grants
+infra grants remove richard@infrahq.com $CIVO_SSH_HOSTNAME
+infra grants remove richard@infrahq.com $DO_SSH_HOSTNAME
+infra grants remove richard@infrahq.com $CIVO_K8S_NAME
+infra grants remove richard@infrahq.com $DO_K8S_NAME
+
+
+echo Removing Richard from Dev Group
+infra groups removeuser richard@infrahq.com Developers
+
+echo Making sure Developers still have Edit Role
+infra grants add -g Developers $CIVO_K8S_NAME --role edit
+infra grants add -g Developers $DO_K8S_NAME --role edit
+
+echo Clean up Richards VM
+# ssh richard@rocky rm /home/richard/.kube/config
+scp /Users/demo/demoprep/client/nginx-richard.yaml richard@rocky:/home/richard
+ssh richard@rocky kubectl delete -f /home/richard/nginx-richard.yaml
+ssh richard@rocky infra logout
+
+echo Reset Complete
+
+# End of the script
