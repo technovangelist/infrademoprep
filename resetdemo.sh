21e009b407e55129d2d5b4be6e3afca69f19737e
Matt Williams <m@technovangelist.com>
2023-01-31 17:24:07 -0800

    modified resetdemo.sh in .
    Signed-off-by: Matt Williams <m@technovangelist.com>

--- resetdemo.sh
+++ resetdemo.sh
@@ -10,8 +10,15 @@ infra grants remove richard@infrahq.com $DO_SSH_HOSTNAME
 infra grants remove richard@infrahq.com $CIVO_K8S_NAME
 infra grants remove richard@infrahq.com $DO_K8S_NAME
 
-echo Removing Richard from Dev Group
+echo Removing Rachels Grants
+infra grants remove rachel@infrahq.com $CIVO_SSH_HOSTNAME
+infra grants remove rachel@infrahq.com $DO_SSH_HOSTNAME
+infra grants remove rachel@infrahq.com $CIVO_K8S_NAME
+infra grants remove rachel@infrahq.com $DO_K8S_NAME
+
+echo Removing Richard and Rachel from Dev Group
 infra groups removeuser richard@infrahq.com Developers
+infra groups removeuser rachel@infrahq.com Developers
 
 echo Making sure Developers still have Edit Role
 infra grants add -g Developers $CIVO_K8S_NAME --role edit
@@ -20,8 +27,13 @@ infra grants add -g Developers $DO_K8S_NAME --role edit
 echo Clean up Richards VM
 # ssh richard@rocky rm /home/richard/.kube/config
 scp /Users/demo/demoprep/client/nginx.yaml richard@rocky:/home/richard
-ssh richard@rocky kubectl remove -f /home/richard/nginx.yaml
+ssh richard@rocky kubectl delete -f /home/richard/nginx.yaml
 ssh richard@rocky infra logout
+
+echo Clean up Rachels VM
+# ssh richard@rocky rm /home/richard/.kube/config
+scp /Users/demo/demoprep/client/nginx.yaml rachel@rocky:/home/rachel
+ssh rachel@rocky infra logout
 
 echo Reset Complete
 
